c7ee5a60bcb828018152e615965077515e61b301
Nasser Ghaleb <63391304+NassCode@users.noreply.github.com>
2023-08-14 00:20:09 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function change() {
+
+  let input = document.getElementById("inpt").value
+
+
+  document.getElementById("para").innerText = input
+document.getElementById("para2").innerText = input
+document.getElementById("para3").innerText = input
+}
